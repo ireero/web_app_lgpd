3292df66eeb476fc00fb0cd53a5af4ed0f0c3153
ireero <renato.memory@gmail.com>
2022-08-03 15:05:34 -0300

    Adicionando alguns comentários, modificando a linguagem do Django para pt-br e modificando o tempo para Brasil - São Paulo.

--- web_app/quiz/admin.py
+++ web_app/quiz/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
 from .models import Usuario
 
+# Registra o Objeto Usuario nas configurações de administração do Django.
 admin.site.register(Usuario)
--- web_app/quiz/models.py
+++ web_app/quiz/models.py
@@ -1,9 +1,11 @@
 from django.db import models
 
 
+# Classe que será utilizada como objeto de configuração no banco de dados
 class Usuario(models.Model):
 
-    def __str__(self):
+    # Metodo para ao vizualisar o banco de dados seja mostrado cada dado com o atributo nome
+    def __str__(self) -> None:
         return self.nome
 
     nome = models.CharField(max_length=100)
