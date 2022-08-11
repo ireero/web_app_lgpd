ce1715a5b746f1d349f59a81bc37a624184ffd22
ireero <78822225+ireero@users.noreply.github.com>
2022-08-11 17:30:51 -0300

    Efetuando a verificação de login através do form e adicionando o módulo de Login previamente escrito ao projeto.

--- web_app/quiz/views.py
+++ web_app/quiz/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from . import models
+from .login import Login
 
 
 def index(request):
@@ -7,4 +8,32 @@ def index(request):
 
 
 def login(request):
-    return render(request, 'quiz/login_register.html')
+
+    context = {}
+
+    if request.method == 'POST':
+
+        log = Login()
+
+        erros = {}
+
+        email = request.POST.get('email', None)
+        senha = request.POST.get('senha', None)
+
+        # Verifica se os campos estão vazios
+        if email == '' and senha == '':
+            erros['erro'] = 'Por favor insira algo para validação!'
+
+        elif email == '':
+            erros['erro'] = 'Por favor insira um e-mail válido!'
+
+        elif senha == '':
+            erros['erro'] = 'Por favor insira uma senha válida'
+
+        if erros:
+            context['erros'] = erros
+
+        print(f'O email digitado foi: {email}\nA senha digitada foi: {senha}')
+        log.verificarLogin(email, senha)
+    return render(request, 'quiz/login_register.html', context)
+
