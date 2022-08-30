ecebc5d7731e6606cac66c20364d7ab7ba2a11ae
ireero <78822225+ireero@users.noreply.github.com>
2022-08-30 14:20:55 -0300

    Modificação nas pontuações baseada nos pesos de cada pergunta.

--- web_app/quiz/modulos/pontuacoes.py
+++ web_app/quiz/modulos/pontuacoes.py
@@ -1,8 +1,13 @@
+from ..models import Pergunta, Questao
 
 class Pontuacoes:
 
     __estrelas = 0
     __pontuacao_parcial = 0
+    
+    def __init__(self) -> None:
+        self.questao = Questao.objects.all()
+        self.perguntas_pesos = Pergunta.objects.all()
 
     # Função utilizada para calcular a quantidade de estrelas que o usuário vai receber
     def calculo_estrela(self) -> None:
@@ -13,16 +18,22 @@ class Pontuacoes:
         self.__estrelas = (5 * self.__pontuacao_parcial) / (9 * 2)  # Calculo das estrelas
 
     # Função utilizada para calcular a pontuação parcial do usuário (100% = 2 pontos, 50% = 1 ponto)
-    def calculo_pontuacao_parcial(self, lista_respostas_qtd) -> None:
+    def calculo_pontuacao_parcial(self, lista_respostas) -> None:
         """
             -> Função utilizada para calcular a pontuação parcial do usuário no teste de adequação
         :return: Não retorna nada
         """
-        for c in lista_respostas_qtd:  # Esse for anda pela lista respostas_qtd e também pelo seu indice
+        for pos, c in enumerate(lista_respostas):  # Esse for anda pela lista respostas_qtd e também pelo seu indice
             if c == 1 or c == 0:  # Verifica as respostas de 50%
-                self.__pontuacao_parcial += 1
+                if self.perguntas_pesos.get(id=pos+1).pesos == 1:
+                    self.__pontuacao_parcial += 1
+                else:
+                    self.__pontuacao_parcial += 2
             elif c == 2:  # Verifica a resposta de 100%
-                self.__pontuacao_parcial += 2
+                if self.perguntas_pesos.get(id=pos+1).pesos == 1:
+                    self.__pontuacao_parcial += 2
+                else:
+                    self.__pontuacao_parcial += 3
 
     # Retorna o valor de estrelas
     def get_estrelas(self):
